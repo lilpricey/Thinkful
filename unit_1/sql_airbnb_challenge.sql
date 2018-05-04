62217c3442ea989363ec32fceba0908ce3215ec0
lilpricey <lorenjprice@gmail.com>
2018-05-04 19:50:14 -0400

    added last question

--- unit_1/sql_airbnb_challenge.sql
+++ unit_1/sql_airbnb_challenge.sql
@@ -15,3 +15,12 @@ FROM
 GROUP BY 1 
 ORDER BY 2 DESC 
 
+SELECT 
+	strftime('%m', date) month,
+	AVG(REPLACE(price, '$', '')) price
+FROM 
+	calendar 
+GROUP BY month
+
+--march is the least expensive month at $36.6, August is the most expensive at $48.3
+
